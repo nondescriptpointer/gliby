8c94cb536e78342714104cf885d018c2a69ea5f6
Thomas Colliers <thomascolliers@gmail.com>
2013-05-16 02:15:17 +0200

    Transformations and interaction fixed

--- include/UIElement.h
+++ include/UIElement.h
@@ -9,7 +9,7 @@ namespace gliby {
 
 class UIElement {
     public:
-        UIElement(const char* location, int width, int height, int x, int y, Math3D::Matrix44f &screenSpace, GLuint shader, bool transparent = true, bool debug = false);
+        UIElement(const char* location, int width, int height, int x, int y, Math3D::Matrix44f &screenSpace, int window_width, int window_height, GLuint shader, bool transparent = true, bool debug = false);
         ~UIElement();
         void resize(int w, int h);
         void mouseUpdate(int x, int y);
@@ -17,13 +17,23 @@ class UIElement {
         void charEvent(int character, int state);
         void keyEvent(int key, int state); 
         void draw();
+        void setX(int x);
+        void setY(int y);
     protected:
         TextureWindow* window;
         Batch* batch;
         bool over;
+        int w;
+        int h;
         int xpos;
         int ypos;
+        int window_w;
+        int window_h;
+        int mousex;
+        int mousey;
         Math3D::Matrix44f *projectionMatrix;
+        Math3D::Matrix44f modelViewMatrix;
+        Math3D::Matrix44f combinedMatrix;
         GLuint shaderObj;
 };
 
--- src/UIElement.cpp
+++ src/UIElement.cpp
@@ -3,18 +3,22 @@
 #include "Math3D.h"
 #include "GeometryFactory.h"
 #include <string.h>
+#include <iostream>
 
 using namespace gliby;
 using namespace Math3D;
 
-UIElement::UIElement(const char* location, int width, int height, int x, int y, Matrix44f &screenSpace, GLuint shader, bool transparent, bool debug):window(NULL),batch(NULL),over(false),xpos(x),ypos(y),shaderObj(shader) {
+UIElement::UIElement(const char* location, int width, int height, int x, int y, Matrix44f &screenSpace, int window_width, int window_height,  GLuint shader, bool transparent, bool debug):window(NULL),batch(NULL),over(false),w(width),h(height),xpos(x),ypos(y),window_w(window_width),window_h(window_height),shaderObj(shader) {
+    // set up matrices
     projectionMatrix = &screenSpace;
+    resize(window_w, window_h);
     // create texture window
     window = new TextureWindow(width,height,transparent,debug);
     window->clear();
+    window->window()->focus();
     window->window()->navigateTo(location,strlen(location));
     // create batch
-    batch = &GeometryFactory::overlay(float(width),float(height),10.0f,210.0f);
+    batch = &GeometryFactory::overlay(float(width),float(height),0.0f,0.0f);
 }
 UIElement::~UIElement(){
     delete window;
@@ -23,22 +27,41 @@ UIElement::~UIElement(){
 
 void UIElement::draw(){
     glUniform1i(glGetUniformLocation(shaderObj,"textureUnit"),0);
-    glUniformMatrix4fv(glGetUniformLocation(shaderObj,"mvpMatrix"), 1, GL_FALSE, *projectionMatrix);
+    glUniformMatrix4fv(glGetUniformLocation(shaderObj,"mvpMatrix"), 1, GL_FALSE, combinedMatrix);
     glBindTexture(GL_TEXTURE_2D, window->texture());
     batch->draw();
 }
 void UIElement::resize(int w, int h){
-
+    window_w = w; window_h = h;
+    // build a new transformation matrix
+    translationMatrix(modelViewMatrix, (float)xpos, (float)window_h-(float)ypos, 0.0f);
+    multiplyMatrix44(combinedMatrix, *projectionMatrix, modelViewMatrix);
 }
 void UIElement::mouseUpdate(int x, int y){
-
+    mousex = x; mousey = y; 
+    // check if mouse is within bounds
+    if(mousex > xpos && mousex < xpos+w && mousey > ypos && mousey < ypos+h){
+       over = true;
+       window->window()->mouseMoved(mousex-xpos,mousey-ypos);
+    }else{
+        over = false;
+    }
 }
 void UIElement::mouseEvent(int id, int state){
-
+    if(over) window->window()->mouseButton(0,state);
 }
 void UIElement::charEvent(int character, int state){
-
+    wchar_t c[2];
+    c[0] = character;
+    c[1] = 0;
+    if(over) window->window()->textEvent(c,1);
 }
 void UIElement::keyEvent(int key, int state){
-
+    
+}
+void UIElement::setX(int x){
+    xpos = x;
+}
+void UIElement::setY(int y){
+    ypos = y;
 }
